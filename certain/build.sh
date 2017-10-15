13627e46d34e25ec919478de099d5310753678ca
codeeply <codeeply@gmail.com>
2017-10-15 23:48:05 +0800

    build static_lib only for travis

--- certain/build.sh
+++ certain/build.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+if [ $# != 1 ] ; then
+    echo "Usage: build.sh [libcertain.a|example]"
+    exit
+fi
+
 set -e  # exit immediately on error
 set -x  # display all commands
 
 git submodule update --init --recursive
 
-# For certain/example.
-sh third/autobuild.sh; make -j 10 example;
+if [ $1 = "libcertain.a" ] ; then
+    if test ! -e third/protobuf/src/.libs/libprotobuf.a; then
+        cd third;
+        cd protobuf; sh ./autogen.sh; ./configure; make -j 4; cd ..;
+        cd ..;
+    fi
+    make -j 4 lib;
+elif [ $1 = "example" ] ; then
+    sh third/autobuild.sh; make -j 4 example;
+else
+    echo "Usage: build.sh [libcertain.a|example]"
+    exit
+fi
